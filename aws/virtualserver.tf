883db5d5746948129ca16be36d7bc16b50aeb752
Shailendra Singh <shailendra_01@outlook.com>
2021-01-25 18:56:39 +0530

    Removed terraform block.

--- aws/virtualserver.tf
+++ aws/virtualserver.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 2.70"
-    }
-  }
-}
-
 provider "aws" {
   profile = "default"
   region  = "us-west-2"
