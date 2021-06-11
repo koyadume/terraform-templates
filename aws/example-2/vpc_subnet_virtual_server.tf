7f73928c47392ad6f77bb78b914ac3276c70a7df
Shailendra Singh <shailendra_01@outlook.com>
2021-06-11 09:35:55 +0530

    Fixed vpc and subnet references.

--- aws/example-2/vpc_subnet_virtual_server.tf
+++ aws/example-2/vpc_subnet_virtual_server.tf
@@ -11,12 +11,13 @@ resource "aws_vpc" "test-vpc" {
   cidr_block = "10.10.0.0/16"
 }
 
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
+resource "aws_subnet" "test-subnet" {
+  vpc_id     = aws_vpc.test-vpc.id
   cidr_block = "10.10.1.0/24"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "test-vm" {
   ami = "ami-invalid"
-  instance_type = "${var.instance_size}"
+  instance_type = "var.instance_size"
+  subnet_id = aws_subnet.test-subnet.id
 }
